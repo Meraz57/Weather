fdfed8ad3099074ea47b06967d21d4b60528b737
Meraz <meraz112233@gmail.com>
2022-05-26 19:18:03 +0600

    xml

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.weather
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import com.example.weather.databinding.ActivityMainBinding
+import com.example.weather.ui.Login
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnagree=findViewById<LinearLayout>(R.id.btnAgree)
+        btnagree.setOnClickListener{
+
+
+        }
+
     }
 }
--- app/src/main/java/com/example/weather/ui/SplashScreen.kt
+++ app/src/main/java/com/example/weather/ui/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.example.weather.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.weather.R
 import com.example.weather.databinding.FragmentSplashScreenBinding
+
 
 class SplashScreen : Fragment() {
 
@@ -32,9 +34,14 @@ class SplashScreen : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnAgree.setOnClickListener {
-            findNavController().navigate(R.id.action_splashScreen_to_login)
+            findNavController().navigate(R.id.action_splashScreen2_to_login)
         }
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
     }
 
 }
